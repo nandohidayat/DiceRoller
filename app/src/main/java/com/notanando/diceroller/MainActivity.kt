023018476903e07c056b5743bdf7f2f4bc727c78
nandohidayat <mnandohidayat@gmail.com>
2020-12-24 18:08:44 +0700

    add button and number

--- app/src/main/java/com/notanando/diceroller/MainActivity.kt
+++ app/src/main/java/com/notanando/diceroller/MainActivity.kt
@@ -2,10 +2,18 @@ package com.notanando.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.notanando.diceroller.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.rollButton.text = "Let's Roll"
     }
 }
